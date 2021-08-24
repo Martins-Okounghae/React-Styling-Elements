1b5a6052e26dcf4235d8ddfe1f4e93fb3262ea54
Martins-Okounghae <martins.okuonghae@outlook.com>
2021-08-24 13:26:05 +0100

    Styling with image tags Styling and adding images

--- src/index.js
+++ src/index.js
@@ -1,14 +1,32 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+const img = "https://picsum.photos/200";
+//const img2 = "https://nigeriankitchenbarrie.com/wp-content/uploads/2021/03/images-13-450x450.jpeg";
 ReactDOM.render(
   <div>
-    <h1>My Favourite Foods</h1>
-    <ul>
-      <li>Bacon</li>
-      <li>Jamon</li>
-      <li>Noodles</li>
-    </ul>
+    <h1 className="heading" contentEditable="false" spellCheck="true">
+      My Favourite Foods
+    </h1>
+
+    <img alt="" src={img + "?grayscale"} />
+    <div>
+      <img
+        alt=""
+        className="food-img"
+        src="https://nigeriankitchenbarrie.com/wp-content/uploads/2021/03/images-13-450x450.jpeg"
+      />
+      <img
+        alt=""
+        className="food-img"
+        src="https://africanvillage.ca/wp-content/uploads/2020/08/1g.jpg"
+      />
+      <img
+        alt=""
+        className="food-img"
+        src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQw6CUCYmJKSvX-5QlCi5lL9562MeMHFOytgw&usqp=CAU"
+      />
+    </div>
   </div>,
   document.getElementById("root")
 );
